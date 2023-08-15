853d3a1666d99e39eca5a14b813fb981ee92c4bd
Zoie <84519205+Zoie601@users.noreply.github.com>
2023-08-15 20:57:14 +0700

    Update __template.cpp

--- __template.cpp
+++ __template.cpp
@@ -2,43 +2,41 @@
  
 using namespace std;
 namespace {
-	#define NAME ""
+  #define NAME ""
 
-	// Types
-	#define ll long long
-	#define ull unsigned long long
-	#define pii pair<int,int>
-	#define pll pair<ll,ll>
+  // Types
+  #define ll long long
+  #define ull unsigned long long
+  #define pii pair<int,int>
+  #define pll pair<ll,ll>
 
-	// Shortcuts
-	#define el cout << "\n"
-	#define ft first
-	#define se second
-	#define pb push_back
-	#define eb emplace_back
-	#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-	#define tcase int t; cin >> t; while(t--)
+  // Shortcuts
+  #define el cout << "\n"
+  #define ft first
+  #define se second
+  #define pb push_back
+  #define eb emplace_back
+  #define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+  #define tcase int t; cin >> t; while(t--)
 
-	// Functions
-	#define all(a) a.begin(), a.end()
-	#define rall(a) a.rbegin(), a.rend()
-	#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-	#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-	void lower(string& s){ for (auto& x: s) if ('A' <= x && x <= 'Z') x += 32; }
-	void upper(string& s){ for (auto& x: s) if ('a' <= x && x <= 'z') x -= 32; }
+  // Functions
+  #define all(a) a.begin(), a.end()
+  #define rall(a) a.rbegin(), a.rend()
+  #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+  #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 
-	// Const
-	const int MOD = 1000000007;
-	const double PI = 3.1415926535897932384626433832795;
-	const ll inf = LLONG_MAX;
+  // Const
+  const ll MOD = 1000000007;
+  const double PI = 3.1415926535897932384626433832795;
+  const ll inf = 1e18;
 }
 
 void solve(){
-
+  
 }
  
 int main (){
-	fio;
-	solve();
-	return 0;
+  fio;
+  solve();
+  return 0;
 }
